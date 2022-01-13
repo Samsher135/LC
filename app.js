90298bd649d376257c712b11f057e6caac702489
Samsher135 <12345samsingh.samu@gmail.com>
2022-01-13 21:54:27 +0530

    all

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const hbs = require('hbs')
 var mysql = require('mysql');
 require('dotenv').config();
+var dateToWords = require("date-to-words");
 const { all } = require('express/lib/application');
 
 var con = mysql.createConnection({
@@ -47,6 +48,7 @@ app.get('/', (req, res)=>{
 res.render('home');
 });
 
+
 app.get('/search', function(req, res) {
     con.query('SELECT * FROM students_details WHERE GR_NO LIKE "%' + req.query.term + '%"',
     function(err, rows, fields) {
@@ -61,14 +63,26 @@ app.get('/search', function(req, res) {
     // res.render('home', all_data);
 });
 
-app.get('/all_data', (req, res)=>{    
-    console.log(req.query.GR_NO); 
-    con.query('SELECT * FROM students_details WHERE GR_NO = ?', [req.query.GR_NO],
+let serial;
+
+app.get('/all_data', (req, res)=>{ 
+    con.query('SELECT students_details.*,counter.serial_no FROM students_details,counter WHERE GR_NO = ?', [req.query.GR_NO],
     function(err, rows, fields) {
     if (err) throw err;
+    serial = rows[0].serial_no;
     res.send(rows);
     });
-    });
+});
+
+function swap(dates){
+    let trail = dates;
+    let mmdd = trail.split('/');
+    let c = mmdd[0];
+    mmdd[0] = mmdd[1];
+    mmdd[1] = c;
+    return mmdd.join('/'); 
+}
+  
 
 app.post('/LC', (req, res)=>{
     // const {reg_num,fname,religion,caste,subcaste, pob,nation,dob,words,institute,doa,progress,conduct,leave,study,till,reason,remarks,CGPI} = req.body;
@@ -86,14 +100,42 @@ app.post('/LC', (req, res)=>{
     let final = leave.reverse().join(" ");
     let final1 = study.reverse().join(" ");
     let final2 = till.reverse().join(" ");
-
     datas.newleave = final;
     datas.newstudy = final1;
     datas.newtill = final2;
+
     let today = new Date().toLocaleDateString().slice(0, 10);
     datas.todaydate = today;
+
+    datas.newdob = swap(req.body.dob);
+    datas.newdoa = swap(req.body.doa);
+    
+    let towords = dateToWords(new Date(req.body.dob));
+    let spitter = towords.split(" ");
+    let b = spitter[0];
+    spitter[0] = spitter[2];
+    spitter[2] = b;
+
+    datas.toword = spitter.join(" ").replace(',', '').replace("the", "of");
+
+    datas.serial_no = serial;
     console.log(datas, "final");
     res.render('LC', datas);
+});
+
+app.get('/update_serial', (req, res)=>{
+    con.query('SELECT serial_no FROM counter WHERE id = 1',
+    function(err, rows, fields) {
+    if (err) throw err;
+    let serial = rows[0].serial_no;
+    serial++;
+    var sql = "UPDATE counter SET serial_no = ? WHERE id = 1";
+    con.query(sql,[serial], function (err, result) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+    });
+    });
+    res.redirect('/');
 });
 
 app.listen(PORT, console.log(
