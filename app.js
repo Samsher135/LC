5894f024e846b1a2574976c67bfe6cf68e21fa2b
Samsher135 <12345samsingh.samu@gmail.com>
2022-03-20 14:20:35 +0530

    port

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var con = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database:process.env.MYSQL_DATABASE
+  database:process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
 
 con.connect(function(err) {
@@ -232,8 +233,6 @@ app.post('/update_serial', async (req, res)=>{
     }
     res.redirect('/');
 });
-
-
 
 
 app.listen(PORT, console.log(
